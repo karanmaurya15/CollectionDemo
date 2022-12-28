24d0f2fef8140cabba469dbb7d490b595959604a
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-28 23:30:22 +0530

    Set Program

---
+++ CollectionDemo/Demo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo
+{
+    internal class Demo
+    {
+        public static void doSetDemo()
+        {
+            var set = new HashSet<string>();
+            set.Add("Saurabh");
+            set.Add("Akash");
+            set.Add("Irbaz");
+            set.Add("KK");
+            HashSet<string>.Enumerator enumerator = set.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+    }
+}
--- CollectionDemo/Program.cs
+++ CollectionDemo/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome to Collection Program");
+            Console.WriteLine("Wellcome to Collection Program\n");
+            Demo.doSetDemo();
         }
     }
 }
