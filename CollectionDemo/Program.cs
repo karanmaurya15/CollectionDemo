78f932d2c37d4d87440caca748f31cab02542c4d
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-28 23:18:46 +0530

    Inital Commit

---
+++ CollectionDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Collection Program");
+        }
+    }
+}
